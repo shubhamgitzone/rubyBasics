b16de9cc2c4abd0416030054fe04e29c234a9ddb
shubham kumar <shubhamkmuz@gmail.com>
2023-10-25 14:44:44 +0530

    first commit

---
+++ girraffe.rb
@@ -0,0 +1,19 @@
+print "hello world!"
+puts ""
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
+# ruby executes code line by line
+# print lets us print text in single line
+# puts apart from printing the given line, also adds a new empty line next.
+
+#-------------Lecture 2 ::: Variables ----------------------------------
+
+character_name = "Mike"
+character_age = "35"
+puts ("There once was a man named " +character_name)
+puts ("he was "+ character_age +" years old.")
+character_name = "Tom"
+puts ("he really liked the name "+character_name)
+puts ("but didn't like being "+character_age +" .")
