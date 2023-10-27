ded1f4b4c2a65f1b9f928b628cd51563c6c9b783
shubham kumar <shubhamkmuz@gmail.com>
2023-10-27 18:46:09 +0530

    Hashes

---
+++ o6hashes.rb
@@ -0,0 +1,19 @@
+#  hashes allows us to store key-value pairs
+#  works like dictionary
+#  similar to hashMap in java
+#  must have unique keys
+#  keys can be of any datatype
+
+states = {
+  "Pennsylvania" => "PA",
+  "New York" => "NY",
+  "Oregon" => "OR",
+  :Orlando => "OL",
+  2 => "CCG"
+}
+
+puts states
+
+puts states["Oregon"]
+puts states[:Orlando]
+puts states[2]
