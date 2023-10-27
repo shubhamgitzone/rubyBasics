68b5ccb287bb57178dcf0b22b933d4a162358d1e
shubham kumar <shubhamkmuz@gmail.com>
2023-10-27 19:47:13 +0530

    better calculator

---
+++ o10betterCalci.rb
@@ -0,0 +1,20 @@
+#-------------Lecture 13 ::: Better Calculator ----------------------------------
+
+puts "Enter first number: "
+num1 = gets.chomp().to_f
+puts "Enter operator: "
+op = gets.chomp()
+puts "Enter first number: "
+num2 = gets.chomp().to_f
+
+if op == "+"
+  puts (num1 + num2)
+elsif op == "-"
+  puts (num1 - num2)
+elsif op == "/"
+  puts (num1 / num2)
+elsif op == "*"
+  puts (num1 * num2)
+else
+  puts ("Invalid operator")
+end
