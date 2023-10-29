8a6534a0a375d002a6365d3285b53814dc542717
shubham kumar <shubhamkmuz@gmail.com>
2023-10-29 17:49:55 +0530

    Guessing game

--- p13GuessingGame.rb
+++ p13GuessingGame.rb
@@ -6,7 +6,7 @@ guess_count = 0
 guess_limit = 3
 out_of_guesses = false
 
-while guess != secret_word && !out_of_guesses
+while guess != secret_word && !out_of_guesses # '&&' , 'and' does same work
   if guess_count < guess_limit
     puts "Enter guess: "
     guess =gets.chomp()
