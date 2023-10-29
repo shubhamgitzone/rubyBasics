21e906317ab5baa244825628805e41bcd32ce02f
shubham kumar <shubhamkmuz@gmail.com>
2023-10-29 17:35:01 +0530

    While loop

---
+++ p12whileLoop.rb
@@ -0,0 +1,7 @@
+#-------------Lecture 15 ::: While loop ----------------------------------
+
+index = 1
+while index <= 5
+  puts index
+  index = index + 1 # same as index +=1 & but not index ++
+end
