0b8b1e0d6e04504b9c27541a62eee0b84b80d614
shubham kumar <shubhamkmuz@gmail.com>
2023-10-29 17:49:20 +0530

    Guessing Game using while loop

---
+++ p13GuessingGame.rb
@@ -0,0 +1,23 @@
+#-------------Lecture 16 ::: Guessing Game ----------------------------------
+
+secret_word = "giraffe"
+guess =""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
+
+while guess != secret_word && !out_of_guesses
+  if guess_count < guess_limit
+    puts "Enter guess: "
+    guess =gets.chomp()
+    guess_count += 1
+  else
+    out_of_guesses = true
+  end
+end
+
+if out_of_guesses
+  puts "you lose"
+else
+  puts "You won!!!"
+end
